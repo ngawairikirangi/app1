c3c348f00330c4178b16f5742d709c90c741abdd
Ngawai <rikirna@g.toiohomai.ac.nz>
2017-07-13 16:20:58 +1200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace app1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Hello Ngawai!");
+        }
+    }
+}
